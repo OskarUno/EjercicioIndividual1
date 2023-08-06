44ada7d3a74d4badf3278a25d353449130b68f2b
OskarUno <oorellana80@gmail.com>
2023-08-06 00:56:40 -0400

    Ejercicio 1 Modulo 5 Se crea una Actividad y 2 fragmentos

--- app/src/main/java/com/awakelab/ejercicioindividual1/FirstFragment.java
+++ app/src/main/java/com/awakelab/ejercicioindividual1/FirstFragment.java
@@ -17,9 +17,6 @@ import com.awakelab.ejercicioindividual1.databinding.FragmentFirstBinding;
  */
 public class FirstFragment extends Fragment {
     private FragmentFirstBinding binding;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -28,7 +25,6 @@ public class FirstFragment extends Fragment {
     private String mParam2;
 
     public FirstFragment() {
-        // Required empty public constructor
     }
 
     /**
